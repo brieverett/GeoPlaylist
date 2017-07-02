620b2e3c592524b07f0524daf4ae0a7c94b19370
Peter Horton <phorton@softstackfactory.org>
2017-07-02 21:48:09 +0000

    first with tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,34 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { LandingPage } from '../pages/landing/landing';
+import { MainPage } from '../pages/main/main';
+import { UserPage } from '../pages/user/user';
+import { SettingsPage } from '../pages/settings/settings';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-@NgModule({
-  declarations: [
+let injections: any[]=[
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    LandingPage,
+    MainPage,
+    UserPage,
+    SettingsPage,
     TabsPage
-  ],
+  ]
+  
+@NgModule({
+  declarations: injections,
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
-  ],
+  entryComponents: injections,
   providers: [
     StatusBar,
     SplashScreen,
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import { MainPage } from '../main/main';
+import { SettingsPage } from '../settings/settings';
+import { UserPage } from '../user/user';
 
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab1Root = MainPage; 
+  tab2Root = SettingsPage;
+  tab3Root = UserPage;
 
   constructor() {
 
